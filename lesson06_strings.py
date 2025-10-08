405875afbcce5729b137357d0b0bb0395356375b
JaydenAngulo <jaydenangulo21@icloud.com>
2025-10-08 16:52:53 +0000

    updated string lesson

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -8,11 +8,12 @@ print(greeting, name)
 second_letter = greeting[1]
 print(second_letter)
 
+# Concatenation:
 message = greeting + " " + name + "!!!!"
 print("Concatenated message:", message)
 
 print()
-
+# Splicing
 word = "Super-cali-fragil-istic-expi-ali-docious"
 print("First letter:", word[0])
 print("Last letter:", word[-1])
@@ -22,7 +23,7 @@ print("Last seven letters:", word[:-7])
 print("Step through every second character:", word[::2])
 print("Reversed, stepping every third letter:", word[::-3])
 
-## Built in functions
+## Built in string functions
 
 country = "Finland"
 length_of_word = len(country)
@@ -31,4 +32,31 @@ print(length_of_word)
 phrase = "spOngEBOB"
 print("\nUppercase:", phrase.upper())
 print("Lowercase:", phrase.lower())
-print("Capitalized:", phrase.capitalize())
+print("Capitalized first letter:", phrase.capitalize())
+print("Title format:", phrase.title())
+
+print()
+
+# Find and replace text
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace("I'm", "You're")
+next_replacement = sentence.replace("goofy", "goober")
+print(sentence)
+print(new_sentence)
+print(next_replacement)
+
+# FORMATTED STRINGS IN PYTHON
+# f-strings allow variables and expressions inside strings
+
+print("\n--- Formatted Strings ---")
+
+name = "Jayden"
+age = 14
+city = "Flemington"
+
+print(f"Hello, my name is {name}. I am {age} years old and I live in {city}.")
+
+# f-strings can do math and function calls inside {}
+
+print(f"Next year, I'll be {age + 1}. My name in uppercase is {name.upper()}.")
+
