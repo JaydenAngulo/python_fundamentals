55b4e88d320bd3cb646a2e8a0a02348450f8db93
JaydenAngulo <jaydenangulo21@icloud.com>
2025-10-15 17:23:52 +0000

    lists

--- lesson07_conditionals.py
+++ lesson07_conditionals.py
@@ -81,3 +81,51 @@ else:
 
 if day is not "Monday":
     print("It's not Monday")
+
+# Challenge 1: Even or Odd
+# Ask the user for a number, and tell them if it’s even or odd.
+# Example output:
+# Enter a number: 7
+# 7 is odd.
+# Hint: use modulus operator
+
+even_or_odd = int(input("Pick a number: ")) % 2
+if even_or_odd == 0:
+    print("The number is even")
+if even_or_odd == 1:
+    print("The number is odd")
+
+# Challenge 2: Password Check
+# Create a variable with a stored password
+# Ask the user to enter a password. 
+# If it matches the stored password, print "Access granted." Otherwise, "Access denied."
+# Example output:
+# Enter password: dolphin
+# Access granted. Access denied.
+# Enter password: swordfish
+# Access granted.
+
+real_password = "Shell$5"
+password_input = input("Enter a password: ")
+if password_input == real_password:
+    print("Access granted")
+else:
+    print("Access denied")
+
+# Challenge 3: Grading System
+# Ask the user for a numeric grade (0-100) and print a letter grade (A,B,C,D,F).
+# Example output:
+# Enter your grade: 81
+# You earned an B.
+
+grade = int(input("Enter your numeric grade: "))
+if grade <= 64:
+    print("You earned an F")
+if grade >= 65 and grade <= 73:
+    print("You earned a D")
+if grade >= 74 and grade <= 82:
+    print("You earned a C")
+if grade >= 83 and grade <= 91:
+    print("You earned a B")
+if grade >= 92:
+    print("You earned an A")
